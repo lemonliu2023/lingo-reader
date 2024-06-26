92f164cd25d714ec32f243ac389be073137e6ff0
hhk-png <fangluoabc@163.com>
2024-06-26 11:44:10 +0800

    feat: parse container.xml

--- packages/epub-parser/src/epub.ts
+++ packages/epub-parser/src/epub.ts
@@ -4,18 +4,47 @@ import { parsexml, ZipFile } from './utils.ts'
 export class EpubFile {
   zip: ZipFile
   mimeFile: string = 'mimetype'
+  mimeType: string = ''
+  rootFile: string = ''
   constructor(public epubFileName: string) {
     // TODO: image root and link root
     this.zip = new ZipFile(epubFileName)
     this.checkMimeType()
+    this.parseContainer()
   }
 
   async checkMimeType() {
     const fileContent = await this.zip.readFile(this.mimeFile)
-    
     if (fileContent.toLowerCase() !== 'application/epub+zip') {
       throw new Error('Unsupported mime type')
     }
+
+    this.mimeType = fileContent.toLowerCase()
+  }
+
+  async parseContainer() {
+    const containerFile = this.zip.getFileName('meta-inf/container.xml')
+    if (!containerFile) {
+      throw new Error('No container.xml in epub file')
+    }
+
+    const containerXml = await this.zip.readFile(containerFile)
+    const xmlContainer = (await parsexml(containerXml)).container
+    if (!xmlContainer || 
+        !xmlContainer.rootfiles || 
+        !xmlContainer.rootfiles.length) {
+      throw new Error('No rootfiles found')
+    }
+
+    const len = xmlContainer.rootfiles.length
+    const rootFile = xmlContainer.rootfiles[len - 1].rootfile[0]
+    const mediaType = rootFile['$']['media-type']
+    const fullPath = rootFile['$']['full-path']
+    if (mediaType !== 'application/oebps-package+xml' || 
+        fullPath.length === 0) {
+      throw new Error('Rootfile in unknow format')
+    }
+    this.rootFile = fullPath
   }
 }
 
--- packages/epub-parser/test/epub.test.ts
+++ packages/epub-parser/test/epub.test.ts
@@ -4,10 +4,16 @@ import { describe, expect, it } from 'vitest'
 import { EpubFile } from '../src/epub.ts'
 
 describe('epubFile', () => {
-  it('will not throw error when checkMimeType', async () => {
-    // alice.epub file path
-    const currentDir = path.dirname(fileURLToPath(import.meta.url))
-    const epubPath = path.resolve(currentDir, './example/alice.epub')
-    expect(() => new EpubFile(epubPath)).not.toThrow()
+  // alice.epub file path
+  const currentDir = path.dirname(fileURLToPath(import.meta.url))
+  const epubPath = path.resolve(currentDir, './example/alice.epub')
+  const epub = new EpubFile(epubPath)
+
+  it('mimeType', async () => {
+    expect(epub.mimeType).toBe('application/epub+zip')
+  })
+
+  it('container file fullpath', () => {
+    expect(epub.rootFile).toBe('19033/content.opf')
   })
 })
