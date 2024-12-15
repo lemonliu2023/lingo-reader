a6a714c06282511739383e4d67aaa76fa3fbb793
hhk-png <fangluoabc@163.com>
2024-12-15 14:39:02 +0800

    build: reader-html build options

--- reader-html/vite.config.ts
+++ reader-html/vite.config.ts
@@ -4,4 +4,26 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const segments = id.split('node_modules/.pnpm/')
+            if (segments.length > 1) {
+              const packageName = segments[1].split('/')[0]
+              let position = 0
+              if (packageName.startsWith('@')) {
+                position = 1
+              }
+              const name = packageName.slice(0, packageName.indexOf('@', position))
+              // if different sub packages hava same return name,
+              //  they will be bundled to one file
+              return `vendor-${name}`
+            }
+          }
+        },
+      },
+    },
+  },
 })
